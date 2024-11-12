eadebac29d4bb02fb8f67ef9560f11951847c9da
Bao Trinh <qubidt@gmail.com>
2024-11-12 13:32:07 -0600

    feat: localize plugin with i18next

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import i18next from "i18next";
 import {
 	type App,
 	type Editor,
@@ -8,6 +9,9 @@ import {
 	PluginSettingTab,
 	Setting,
 } from "obsidian";
+import { resources } from "./i18n";
+
+const t = i18next.t;
 
 // Remember to rename these classes and interfaces!
 
@@ -23,15 +27,22 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
+		await i18next.init({
+			lng: window.i18next?.language || window.moment?.locale(),
+			fallbackLng: "en",
+			resources,
+			returnNull: false,
+		});
+
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon(
 			"dice",
-			"Sample Plugin",
+			t("plugin.name"),
 			(evt: MouseEvent) => {
 				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
+				new Notice(t("interface.msg-sample-ribbon-icon-clicked"));
 			},
 		);
 		// Perform additional things with the ribbon
@@ -39,12 +50,12 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
+		statusBarItemEl.setText(t("interface.sample-plugin-status-text"));
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
+			name: t("commands.open-sample-modal-simple"),
 			callback: () => {
 				new SampleModal(this.app).open();
 			},
@@ -52,16 +63,17 @@ export default class MyPlugin extends Plugin {
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: "sample-editor-command",
-			name: "Sample editor command",
+			name: t("commands.sample-editor-command"),
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
+				editor.replaceSelection(t("commands.sample-editor-command"));
 			},
 		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
+			name: t("commands.open-sample-modal-complex"),
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
 				const markdownView =
@@ -108,7 +120,7 @@ export default class MyPlugin extends Plugin {
 class SampleModal extends Modal {
 	onOpen() {
 		const { contentEl } = this;
-		contentEl.setText("Woah!");
+		contentEl.setText(t("interface.msg-sample-command"));
 	}
 
 	onClose() {
@@ -131,11 +143,11 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
+			.setName(t("setting.option-setting1"))
+			.setDesc(t("setting.option-setting1-desc"))
 			.addText((text) =>
 				text
-					.setPlaceholder("Enter your secret")
+					.setPlaceholder(t("setting.option-setting1-placeholder"))
 					.setValue(this.plugin.settings.mySetting)
 					.onChange(async (value) => {
 						this.plugin.settings.mySetting = value;
