2a9a4c27cd82a6513ca4633976a2e157237d61d2
Bao Trinh <qubidt@gmail.com>
2024-11-15 08:37:12 -0600

    chore: add log messages

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,14 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
+		console.debug("loading Sample plugin");
 		await this.loadSettings();
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
 
-	onunload() {}
+	onunload() {
+		console.debug("unloading Sample plugin");
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
